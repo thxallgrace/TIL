ef8a6e3a63acc04a7155e6742c3a69efecf08d00
thxallgrace <thxall92@gmail.com>
2022-04-18 10:42:43 +0900

    [programmers] 키패드 누르기 풀이 추가 (3)

--- CodingTest/Programmers/키패드 누르기.kt
+++ CodingTest/Programmers/키패드 누르기.kt
@@ -6,37 +6,44 @@ import kotlin.math.abs
     url     : https://programmers.co.kr/learn/courses/30/lessons/67256
  */
 
-import kotlin.math.abs
-
 class Solution {
     fun solution(numbers: IntArray, hand: String): String {
-        val keypads = mapOf(1 to Pair(0,0), 2 to Pair(0,1), 3 to Pair(0,2),
-                4 to Pair(1,0), 5 to Pair(1,1), 6 to Pair(1,2),
-                7 to Pair(2,0), 8 to Pair(2,1), 9 to Pair(2,2),
-                "*" to Pair(3,0), 0 to Pair(3,1), "#" to Pair(3,2))
 
-        var leftPosition = keypads["*"]
-        var rightPosition = keypads["#"]
+        // We replace '*' to 10, '#' to 12, '0' to 11
+        var leftPosition = 10
+        var rightPosition = 12
 
         var answer = ""
         numbers.forEach { number ->
-            when(number){
+            var n = if (number == 0) 11 else number
+
+            when (n) {
                 1, 4, 7 -> {
                     answer += 'L'
-                    leftPosition = keypads[number]
+                    leftPosition = n
                 }
                 3, 6, 9 -> {
                     answer += 'R'
-                    rightPosition = keypads[number]
+                    rightPosition = n
                 }
-                else ->{
-                    val currentHand = searchNumberHand(leftPosition!!, rightPosition!!, keypads[number]!!, hand == "right")
-                    if (currentHand == 'R') {
-                        rightPosition = keypads[number]
+                else -> {
+                    val leftMove = calcDistance(abs(n - leftPosition))
+                    val rightMove = calcDistance(abs(n - rightPosition))
+                    if (leftMove > rightMove) {
+                        answer += 'R'
+                        rightPosition = n
+                    } else if (rightMove > leftMove) {
+                        answer += 'L'
+                        leftPosition = n
                     } else {
-                        leftPosition = keypads[number]
+                        if (hand == "right") {
+                            answer += 'R'
+                            rightPosition = n
+                        } else {
+                            answer += 'L'
+                            leftPosition = n
+                        }
                     }
-                    answer += currentHand
                 }
 
             }
@@ -44,14 +51,13 @@ class Solution {
         return answer
     }
 
-    fun searchNumberHand(left: Pair<Int,Int>, right: Pair<Int,Int>, target: Pair<Int,Int>, isRightHand: Boolean): Char {
-        val leftMove = abs(left.first - target.first) + abs(left.second - target.second)
-        val rightMove = abs(right.first - target.first) + abs(right.second - target.second)
-
-        if (leftMove == rightMove) {
-            return if (isRightHand) 'R' else 'L'
+    fun calcDistance(number: Int): Int {
+        when (number) {
+            1, 3 -> return 1
+            2, 4, 6 -> return 2
+            5, 7, 9 -> return 3
+            8, 10 -> return 4
+            else -> return 0
         }
-
-        return if (leftMove > rightMove) 'R' else 'L'
     }
-}}
+}
